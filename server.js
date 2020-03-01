5574232727ace52988f69e6c837620b53d363ad6
admase <39431081+admase@users.noreply.github.com>
2020-02-29 18:26:26 -1000

    setup the READ route with user's id

--- server.js
+++ server.js
@@ -1,26 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const port = 8000;
 const app = express();
-const User = require('./models/User');
-mongoose.connect('mongodb://localhost/userData');
+const User = require("./models/User");
+mongoose.connect("mongodb://localhost/userData");
 
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-    console.log(`server is listening on port:${port}`)
-})
+    console.log(`server is listening on port:${port}`);
+});
 
 // CREATE
-app.post('/users', (req, res) => {
+app.post("/users", (req, res) => {
     // User.create()
-})
+    User.create({
+            name: req.body.newData.name,
+            email: req.body.newData.email,
+            password: req.body.newData.password
+        },
+        (err, data) => {
+            if (err) {
+                res.json({ success: false, message: err });
+            } else if (!data) {
+                res.json({ success: false, message: "Not Found" });
+            } else {
+                res.json({ success: true, data: data });
+            }
+        }
+    );
+});
 
-app.route('/users/:id')
+app.route("/users/:id")
     // READ
     .get((req, res) => {
         // User.findById()
+        User.findById(req.param.id, (err, data) => {
+            if (err) {
+                res.json({
+                    success: false,
+                    message: err
+                });
+            } else if (!data) {
+                res.json({
+                    success: false,
+                    message: "Not Found"
+                });
+            } else {
+                res.json({
+                    success: true,
+                    data: data
+                });
+            }
+        });
     })
     // UPDATE
     .put((req, res) => {
@@ -29,4 +62,4 @@ app.route('/users/:id')
     // DELETE
     .delete((req, res) => {
         // User.findByIdAndDelete()
-    })
+    });
